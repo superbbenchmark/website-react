0296030e9c15683035e865be5d120fbbab699384
Leo Yang <leo19941227@gmail.com>
2021-04-12 03:03:29 +0800

    add information

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Route,
 } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
+import { Typography } from '@material-ui/core';
 
 const theme = createMuiTheme({
   palette: {
@@ -29,7 +30,13 @@ function App() {
           <NavigationBar />
           <Switch>
             <Route path="/" exact>
-              <h1>SUPERB Benchmark</h1>
+              <h1>Speech processing Universal PERformance Benchmark</h1>
+              <Typography>
+                This is the official website for the challenge SUPERB (Speech processing Universal PERformance Benchmark).
+                We are working on the challenge rules, scripts for getting started and submitting results, and leaderboard.
+                We will provide more information in mid April.
+                Stay tuned!
+              </Typography>
             </Route>
             <Route path="/users">
               <p>test 2</p>
