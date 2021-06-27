e5937b10155e60220569676fbd84021e4fabd36d
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-06-28 01:22:15 +0800

    add db and login api

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,22 @@
-import flask
-from flask import Flask
+from flask import Flask, request
+from db import db
+from models.user import UserModel
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/superb"
 
-@app.route("/login")
+
+@app.route("/api/login", methods=['POST'])
 def login():
+    # print(request.get_json())
+    req = request.get_json()
+    user = UserModel(name=req['name'], email=req['email'])
+    user.save_to_db()
     return {"message": "Hello world"}, 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    db.init_app(app)
+    app.run(port=5000, debug=True)
