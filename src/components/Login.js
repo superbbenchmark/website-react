782c8860ed8e627c24bbffa3dcf1a7bebdea34a6
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-06-28 18:03:56 +0800

    add connect google login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,40 @@
-import GoogleButton from "react-google-button";
+import { GoogleLogin } from "react-google-login";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth-context";
+
+const clientId =
+    "796679159105-6335p2q2ub5pr15lnf3g2cqkhnucmvkl.apps.googleusercontent.com";
 
 const Login = () => {
-    return <GoogleButton style={{ marginLeft: "auto", marginRight: "auto" }} />;
+    const auth = useContext(AuthContext);
+
+    const onSuccess = async (res) => {
+        console.log(res);
+        console.log(res.tokenId);
+        try {
+            const responseData = await axios.post(
+                "http://localhost:5000/api/login",
+                { id_token: res.tokenId }
+            );
+            auth.login(responseData.data.access_token);
+        } catch (err) {
+            console.log(err.response);
+        }
+    };
+
+    const onFailure = async () => {};
+
+    return (
+        <GoogleLogin
+            onSuccess={onSuccess}
+            clientId={clientId}
+            buttonText="Sign in with Google"
+            theme="dark"
+            responseType="id_token"
+            isSignedIn={true}
+            uxMode="redirect"
+        />
+    );
 };
 export default Login;
