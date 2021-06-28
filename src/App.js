301b8404fddbf46b3eb27950f3e074e97ca1bac1
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-06-28 12:38:58 +0800

    add google login button style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Box, Typography } from "@material-ui/core";
-import { useTheme, ThemeProvider, makeStyles, createMuiTheme } from "@material-ui/core/styles";
+import {
+    useTheme,
+    ThemeProvider,
+    makeStyles,
+    createMuiTheme,
+} from "@material-ui/core/styles";
 
 import Landing from "./Landing";
 import Tasks from "./Tasks";
@@ -10,72 +15,87 @@ import Submit from "./Submit";
 import Compare from "./Compare";
 import Leaderboard from "./Leaderboard";
 import NavigationBar from "./components/NavigationBar";
+import Login from "./components/Login";
 import { mainTheme } from "./components/Theme";
 
 const useStyles = makeStyles((theme) => ({
-  narrowViewport: {
-    width: "85%",
-    maxWidth: 900,
-    margin: "auto",
-  },
+    narrowViewport: {
+        width: "85%",
+        maxWidth: 900,
+        margin: "auto",
+    },
+    LoginButton: {
+        height: "30vh",
+        paddingTop: "10vh",
+    },
 }));
 
 function App() {
-  const [ width, setWidth ] = React.useState(0);
-  const [ height, setHeight ] = React.useState(0);
-  const [ navbarHeight, setNavbarHeight ] = React.useState(0);
-  const tableControlRef = React.useRef(null);
+    const [width, setWidth] = React.useState(0);
+    const [height, setHeight] = React.useState(0);
+    const [navbarHeight, setNavbarHeight] = React.useState(0);
+    const tableControlRef = React.useRef(null);
 
-  const setViewPort = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-    setNavbarHeight(document.getElementById("navbar").offsetHeight);
-  }
-  React.useEffect(setViewPort);
-  window.addEventListener("resize", setViewPort);
+    const setViewPort = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+        setNavbarHeight(document.getElementById("navbar").offsetHeight);
+    };
+    React.useEffect(setViewPort);
+    window.addEventListener("resize", setViewPort);
 
-  const classes = useStyles();
-  const theme = useTheme();
-  return (
-    <div className="App">
-      <Router>
-        <div id="navbar">
-          <NavigationBar tableControlRef={tableControlRef} />
+    const classes = useStyles();
+    const theme = useTheme();
+    return (
+        <div className="App">
+            <Router>
+                <div id="navbar">
+                    <NavigationBar tableControlRef={tableControlRef} />
+                </div>
+                <Switch>
+                    <Route path="/" exact>
+                        <div className={`${classes.narrowViewport}`}>
+                            <Landing />
+                        </div>
+                    </Route>
+                    <Route path="/tasks">
+                        <div className={`${classes.narrowViewport}`}>
+                            <Tasks />
+                        </div>
+                    </Route>
+                    <Route path="/submit">
+                        <div className={`${classes.narrowViewport}`}>
+                            <Submit />
+                        </div>
+                    </Route>
+                    <Route path="/compare">
+                        <div className={`${classes.narrowViewport}`}>
+                            <Compare />
+                        </div>
+                    </Route>
+                    <Route path="/login">
+                        <div
+                            className={`${classes.narrowViewport} ${classes.LoginButton}`}
+                        >
+                            <Login />
+                        </div>
+                    </Route>
+                    <Route path="/leaderboard">
+                        <Leaderboard
+                            height={`${height - navbarHeight}px`}
+                            tableControlRef={tableControlRef}
+                        />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
-        <Switch>
-          <Route path="/" exact>
-            <div className={`${classes.narrowViewport}`}>
-              <Landing />
-            </div>
-          </Route>
-          <Route path="/tasks">
-            <div className={`${classes.narrowViewport}`}>
-              <Tasks />
-            </div>
-          </Route>
-          <Route path="/submit">
-            <div className={`${classes.narrowViewport}`}>
-              <Submit />
-            </div>
-          </Route>
-          <Route path="/compare">
-            <div className={`${classes.narrowViewport}`}>
-              <Compare />
-            </div>
-          </Route>
-          <Route path="/leaderboard">
-            <Leaderboard height={`${height - navbarHeight}px`} tableControlRef={tableControlRef} />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default () => {
-  return (
-    <ThemeProvider theme={createMuiTheme(mainTheme)}>
-      <App />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={createMuiTheme(mainTheme)}>
+            <App />
+        </ThemeProvider>
+    );
 };
---
+++ src/components/Login.js
@@ -0,0 +1,6 @@
+import GoogleButton from "react-google-button";
+
+const Login = () => {
+    return <GoogleButton style={{ marginLeft: "auto", marginRight: "auto" }} />;
+};
+export default Login;
