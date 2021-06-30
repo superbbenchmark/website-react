0f7eb1441addc92778e91badf704336870b55fa0
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-06-30 11:34:01 +0800

    add file upload helper

---
+++ backend/file_upload.py
@@ -0,0 +1,34 @@
+import os
+import magic
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'zip'}
+ALLOWED_MIME_TYPES = {'application/zip'}
+
+
+def zipfile_check(file):
+    if '.' in file.filename:
+        ext = file.filename.rsplit('.', 1)[1].lower()
+    else:
+        return False
+
+    mime_type = magic.from_buffer(file.stream.read(), mime=True)
+    if (
+        mime_type in ALLOWED_MIME_TYPES and
+        ext in ALLOWED_EXTENSIONS
+    ):
+        # move the cursor to the beginning
+        file.stream.seek(0, 0)
+        return True
+
+    return False
+
+
+def create_folder(email, upload_count):
+    folder = os.path.join("upload", email, upload_count)
+    os.makedirs(folder, exist_ok=True)
+    return folder
+
+
+def get_path(folder, file_name):
+    return os.path.join(folder, secure_filename(file_name))
