713087c6ce463f5073f121d49928f485aab6b92d
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-06-30 11:34:46 +0800

    finish file upload

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-from os import access
 from flask import Flask, request, jsonify
 from db import db
 from models.user import UserModel
@@ -6,8 +5,10 @@ from models.files import FileModel
 import google_token
 from http import HTTPStatus
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import datetime
 from flask_cors import CORS
 from utils import get_leaderboard
+import file_upload
 
 app = Flask(__name__)
 
@@ -15,6 +16,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/superb"
 app.config['GOOGLE_CLIENT_ID'] = '796679159105-6335p2q2ub5pr15lnf3g2cqkhnucmvkl.apps.googleusercontent.com'
 app.config['JWT_SECRET_KEY'] = 'speechlab531'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Max file size: 16MB
+app.config['UPLOAD_DIR'] = "./upload"
 
 jwt = JWTManager(app)
 CORS(app)
@@ -46,28 +49,53 @@ def login():
         if not UserModel.find_by_email(email=identity['email']):
             user = UserModel(email=identity['email'], name=identity['name'])
             user.save_to_db()
-        access_token = create_access_token(identity=identity['email'])
+        access_token = create_access_token(
+            identity=identity['email'], expires_delta=datetime.timedelta(hours=1))
         return {"message": "Login Success", "access_token": access_token}, HTTPStatus.OK
 
     except Exception as e:
         print(e)
         return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
+
 @app.route("/api/result/leaderboard", methods=['GET'])
 def leaderboard_request():
     try:
         leaderboard_data = get_leaderboard()
-        return jsonify({"leaderboard" : leaderboard_data}), HTTPStatus.OK
+        return jsonify({"leaderboard": leaderboard_data}), HTTPStatus.OK
 
     except Exception as e:
         print(e)
         return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
+
 @app.route("/api/result/upload", methods=["POST"])
 @jwt_required()
 def result_upload():
-    user_mail = get_jwt_identity()
-    return {"message": user_mail}, 200
+    try:
+        user_mail = get_jwt_identity()
+        file = request.files['file']
+
+        if file.filename == "":
+            return {"msg": "No file selected."}, HTTPStatus.FORBIDDEN
+        if not file_upload.zipfile_check(file):
+            return {"msg": "Wrong file format."}, HTTPStatus.FORBIDDEN
+
+        upload_count = FileModel.get_upload_count_by_mail(email=user_mail) + 1
+        folder = file_upload.create_folder(user_mail, str(upload_count))
+        file_path = file_upload.get_path(folder, file.filename)
+        fileObj = FileModel(
+            email=user_mail, submitName="Test", filePath=file_path)
+        fileObj.save_to_db()
+
+        try:
+            file.save(file_path)
+            return {"msg": "Upload Success!"}, HTTPStatus.OK
+        except Exception as e:
+            fileObj.delete_from_db()  # Rollback
+            return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
+    except:
+        return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 if __name__ == '__main__':
--- backend/models/files.py
+++ backend/models/files.py
@@ -14,14 +14,16 @@ class FileModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.ForeignKey('users.email'), nullable=False)
-    state = db.Column(db.Enum(Status))
-    filePath = db.Column(db.String(264))
-    SubmitName = db.Column(db.String(80))
+    submitName = db.Column(db.String(80),  nullable=False)
+    state = db.Column(db.Enum(Status),  nullable=False,
+                      default=Status.UPLOADED)
+    filePath = db.Column(db.String(264), nullable=False)
     ModelURL = db.Column(db.String(264))
     ModelDescription = db.Column(db.Text())
     TotalScore = db.Column(db.Float)
     PRscore = db.Column(db.Float)
     ASRscore = db.Column(db.Float)
+    date_upload = db.Column(db.DateTime,  default=db.func.current_timestamp())
 
     @classmethod
     def find_by_email(cls, email: str) -> "FileModel":
@@ -30,6 +32,10 @@ class FileModel(db.Model):
     @classmethod
     def find_all(cls) -> List["FileModel"]:
         return cls.query.all()
+
+    @classmethod
+    def get_upload_count_by_mail(cls, email: str) -> int:
+        return cls.query.filter_by(email=email).count()
 
     def save_to_db(self) -> None:
         db.session.add(self)
