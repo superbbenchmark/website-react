80e27ac7316732ba1ca2512579a6d891d58506e6
Kaminyou <ikaminyou@gmail.com>
2021-07-06 00:07:15 +0800

    required zip.

--- backend/prepare.sh
+++ backend/prepare.sh
@@ -1,4 +1,3 @@
 sudo apt-get install default-jre
 pip install -r requirements.txt
-mkdir examples && cd examples && gdown --id 10Vg76oFxHXfdYJ5fR5Yg3P2F9Qx7QwyM
-cd inference && gdown --id 17zfIIcSmhCrGOiOjr5vzvvNjgzIxJD4s && unzip truth.zip
+cd inference && unzip truth.zip
