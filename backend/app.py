41d8b6aab0ba4a20c10851b4bf424aa8d612e614
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-07-09 01:15:13 +0800

    move api to resources

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,13 @@
-from flask_restx import Api
-from config import configs
-from resources.user import UserInfo
-from resources.file import LeaderBoard, Result
-import os
-from dotenv import load_dotenv
+from flask import Flask
 from flask_cors import CORS
-import datetime
-from flask_jwt_extended import JWTManager, create_access_token
-from flask import Flask, request, send_file
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+from flask_restx import Api
 from db import db
-from models.user import UserModel
-import google_token
-from http import HTTPStatus
+from resources.user import UserInfo, UserLogin
+from resources.result import LeaderBoard, Result
+from resources.dowload import Example
 
 app = Flask(__name__)
 load_dotenv()
@@ -25,7 +21,6 @@ app.config['JWT_SECRET_KEY'] = 'speechlab531'
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # Max file size: 16MB
 app.config['UPLOAD_DIR'] = "./upload"
 
-mapping = {"constrained": 1, "less-constrained": 2, "unconstrained": 3}
 
 jwt = JWTManager(app)
 CORS(app)
@@ -37,121 +32,11 @@ def create_tables():
     db.create_all()
 
 
-@app.route("/api/login", methods=['POST'])
-def login():
-    try:
-        if 'id_token' not in request.get_json():
-            return {"msg": "No Google ID token provided"}, HTTPStatus.FORBIDDEN
-
-        token = request.get_json()['id_token']
-
-        try:
-            identity = google_token.validate_id_token(
-                token, app.config['GOOGLE_CLIENT_ID'])
-        except ValueError:
-            return {"msg": 'Invalid Google ID token'}, HTTPStatus.FORBIDDEN
-
-        # Get the user info out of the validated identity
-        if ('email' not in identity or 'name' not in identity):
-            return {"msg": "Unexcpected authorization response"}, HTTPStatus.FORBIDDEN
-
-        if not UserModel.find_by_email(email=identity['email']):
-            user = UserModel(email=identity['email'], name=identity['name'])
-            user.save_to_db()
-        access_token = create_access_token(
-            identity=identity['email'], expires_delta=datetime.timedelta(hours=1))
-        return {"msg": "Login Success", "access_token": access_token}, HTTPStatus.OK
-
-    except Exception as e:
-        print(e)
-        return {"msg": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-# @app.route("/api/result/leaderboard", methods=['GET'])
-# def leaderboard_request():
-#     try:
-#         leaderboard_default_data = get_leaderboard_default()
-#         leaderboard_user_data = FileModel.find_show_on_leaderboard()
-#         submission_names = []
-#         for user_data in leaderboard_user_data:
-#             submission_names.append(
-#                 UserModel.find_by_email(email=user_data.email).name)
-#         submission_info = submission_records_parser(
-#             leaderboard_user_data, configs, mode="leaderboard")
-
-#         for single_info, name in zip(submission_info, submission_names):
-#             single_info.update({"name": name})
-
-#         leaderboard_default_data += submission_info
-
-#         return jsonify({"leaderboard": leaderboard_default_data}), HTTPStatus.OK
-
-#     except Exception as e:
-#         print(e)
-#         return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-# @app.route("/api/profile/quota", methods=["GET"])
-# @jwt_required()
-# def get_user_quota():
-#     try:
-#         user_mail = get_jwt_identity()
-#         daily_counts = FileModel.get_interval_upload_count_by_mail(
-#             email=user_mail, AOEtime=get_AOE_today(to_str=False))
-#         monthly_counts = FileModel.get_interval_upload_count_by_mail(
-#             email=user_mail, AOEtime=get_AOE_month(to_str=False))
-#         daily_left = configs["DAILY_SUBMIT_LIMIT"] - daily_counts
-#         monthly_left = configs["MONTHLY_SUBMIT_LIMIT"] - monthly_counts
-#         return {"mgs": "Your quota:", "daily_counts": daily_counts, "monthly_counts": monthly_counts, "daily_left": daily_left, "monthly_left": monthly_left}, HTTPStatus.OK
-
-#     except Exception as e:
-#         print(e)
-#         return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-# @app.route("/api/profile/username", methods=["GET"])
-# @jwt_required()
-# def get_username():
-#     try:
-#         user_mail = get_jwt_identity()
-#         user = UserModel.find_by_email(email=user_mail)
-#         return jsonify({"username": user.name}), HTTPStatus.OK
-
-#     except Exception as e:
-#         print(e)
-#         return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-# @app.route("/api/profile/resetusername", methods=["POST"])
-# @jwt_required()
-# def reset_username():
-#     try:
-#         user_mail = get_jwt_identity()
-#         data = request.get_json()
-#         newusername = data["newusername"]
-#         if (len(newusername) == 0):
-#             return jsonify({"msg": "Too short!"}), HTTPStatus.FORBIDDEN
-
-#         UserModel.reset_username(email=user_mail, new_name=newusername)
-#         return jsonify({"msg": newusername}), HTTPStatus.OK
-
-#     except Exception as e:
-#         print(e)
-#         return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-@app.route("/api/download/example", methods=['GET'])
-def download_example():
-    try:
-        return send_file("./examples/predict.zip", as_attachment=True)
-
-    except Exception as e:
-        return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-
 api.add_resource(Result, "/api/result")
 api.add_resource(LeaderBoard, "/api/result/leaderboard")
 api.add_resource(UserInfo, "/api/user/info")
+api.add_resource(UserLogin, "/api/user/login")
+api.add_resource(Example, "/api/download/example")
 
 if __name__ == '__main__':
     db.init_app(app)
