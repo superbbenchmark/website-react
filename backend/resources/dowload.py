f231237336c2620ccfeedcd1b70e1dc12871635b
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-07-09 01:14:24 +0800

    add download and submission resources

---
+++ backend/resources/dowload.py
@@ -0,0 +1,12 @@
+from flask_restx import Resource
+from flask import send_file
+from http import HTTPStatus
+
+
+class Example(Resource):
+    @classmethod
+    def get(cls):
+        try:
+            return send_file("./examples/predict.zip", as_attachment=True)
+        except Exception as e:
+            return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,11 +1,13 @@
 from flask_restx import Resource
 from flask import request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+import datetime
 from http import HTTPStatus
 
 from models.user import UserModel
 from models.file import FileModel
 from utils import get_AOE_today, get_AOE_month
+import google_token
 from config import configs
 
 
@@ -13,6 +15,7 @@ class UserInfo(Resource):
     @classmethod
     @jwt_required()
     def get(cls):
+        '''Get user personal info (userName, dialy count, monthly counts, daily left, monthly left)'''
         try:
             user_mail = get_jwt_identity()
             user = UserModel.find_by_email(email=user_mail)
@@ -32,6 +35,7 @@ class UserInfo(Resource):
     @classmethod
     @jwt_required()
     def patch(cls):
+        '''Update User Name'''
         try:
             user_mail = get_jwt_identity()
             data = request.get_json()
@@ -45,3 +49,36 @@ class UserInfo(Resource):
         except Exception as e:
             print(e)
             return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        '''User Google Login'''
+        try:
+            if 'id_token' not in request.get_json():
+                return {"msg": "No Google ID token provided"}, HTTPStatus.FORBIDDEN
+
+            token = request.get_json()['id_token']
+
+            try:
+                identity = google_token.validate_id_token(
+                    token, configs['GOOGLE_CLIENT_ID'])
+            except ValueError:
+                return {"msg": 'Invalid Google ID token'}, HTTPStatus.FORBIDDEN
+
+            # Get the user info out of the validated identity
+            if ('email' not in identity or 'name' not in identity):
+                return {"msg": "Unexcpected authorization response"}, HTTPStatus.FORBIDDEN
+
+            if not UserModel.find_by_email(email=identity['email']):
+                user = UserModel(
+                    email=identity['email'], name=identity['name'])
+                user.save_to_db()
+            access_token = create_access_token(
+                identity=identity['email'], expires_delta=datetime.timedelta(hours=1))
+            return {"msg": "Login Success", "access_token": access_token}, HTTPStatus.OK
+
+        except Exception as e:
+            print(e)
+            return {"msg": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
