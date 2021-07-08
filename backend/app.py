402af7c716f0a056052f7b37627fa9f2ae975e7c
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-07-08 22:27:37 +0800

    refactor and add file flask resources

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,37 @@
 from flask import Flask, request, jsonify, send_file
-from threading import Thread
 from db import db
 from models.user import UserModel
-from models.file import FileModel, Task, Show
-from models.score import ScoreModel
+from models.file import FileModel
 import google_token
 from http import HTTPStatus
+from flask_restful import Api
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import datetime
 from flask_cors import CORS
-from utils import get_leaderboard_default, get_AOETime, get_uuid, submission_records_parser, get_AOE_today, get_AOE_month
-import file_upload
-from calculate import metric_calculate_pipeline
+from utils import get_leaderboard_default, submission_records_parser, get_AOE_today, get_AOE_month
 from dotenv import load_dotenv
 import os
-import yaml
+
+from resources.file import Result
+from config import configs
 
 app = Flask(__name__)
 load_dotenv()
 
-with open("configs.yaml") as f:
-    configs = yaml.safe_load(f)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', default="mysql+pymysql://root:root@127.0.0.1:3306/superb")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'SQLALCHEMY_DATABASE_URI', default="mysql+pymysql://root:root@127.0.0.1:3306/superb")
 app.config['GOOGLE_CLIENT_ID'] = '796679159105-6335p2q2ub5pr15lnf3g2cqkhnucmvkl.apps.googleusercontent.com'
 app.config['JWT_SECRET_KEY'] = 'speechlab531'
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # Max file size: 16MB
 app.config['UPLOAD_DIR'] = "./upload"
 
-mapping = {"constrained":1, "less-constrained":2, "unconstrained":3}
+mapping = {"constrained": 1, "less-constrained": 2, "unconstrained": 3}
 
 jwt = JWTManager(app)
 CORS(app)
+api = Api(app)
 
 
 @app.before_first_request
@@ -77,11 +76,13 @@ def leaderboard_request():
         leaderboard_user_data = FileModel.find_show_on_leaderboard()
         submission_names = []
         for user_data in leaderboard_user_data:
-            submission_names.append(UserModel.find_by_email(email=user_data.email).name)
-        submission_info = submission_records_parser(leaderboard_user_data, configs, mode = "leaderboard")
-        
+            submission_names.append(
+                UserModel.find_by_email(email=user_data.email).name)
+        submission_info = submission_records_parser(
+            leaderboard_user_data, configs, mode="leaderboard")
+
         for single_info, name in zip(submission_info, submission_names):
-            single_info.update( {"name":name})
+            single_info.update({"name": name})
 
         leaderboard_default_data += submission_info
 
@@ -91,137 +92,24 @@ def leaderboard_request():
         print(e)
         return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
-@app.route("/api/result/individual", methods=["GET"])
-@jwt_required()
-def individual_upload():
-    try:
-        user_mail = get_jwt_identity()
-        submission_records = FileModel.find_by_email(email=user_mail).all()
-        submission_info = submission_records_parser(submission_records, configs, mode="individual")
-
-        return jsonify({"submission_info": submission_info}), HTTPStatus.OK
-    except Exception as e:
-        print(e)
-        return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-@app.route("/api/result/shown", methods=["POST"])
-@jwt_required()
-def set_shown_result():
-    try:
-        user_mail = get_jwt_identity()
-        data = request.get_json()
-        task = data["task"]
-        submitID = data["submission_id"]
-        submission_record = FileModel.find_by_submitID(submitUUID=submitID)
-
-        assert submission_record.email == user_mail
-        task_id = submission_record.task.value #== mapping[task]
-
-        if submission_record.showOnLeaderboard == Show.YES:
-            # set the "show" of all the same task submission to "NO"
-            FileModel.reset_same_task_show_attribute(email=user_mail, task=Task(task_id))
-            return jsonify({"msg": "Remove from the leaderboard!", "submitID":submitID}), HTTPStatus.OK
-        
-        else:
-            # set the "show" of all the same task submission to "NO"
-            FileModel.reset_same_task_show_attribute(email=user_mail, task=Task(task_id))
-            FileModel.set_show_attribute_by_submitID(submitUUID=submitID)
-            return jsonify({"msg": "Shown on the leaderboard!", "submitID":submitID}), HTTPStatus.OK
-
-    except Exception as e:
-        return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-@app.route("/api/result/upload", methods=["POST"])
-@jwt_required()
-def result_upload():
-    try:
-        user_mail = get_jwt_identity()
-
-        # check current submission counts
-        daily_counts = FileModel.get_interval_upload_count_by_mail(email=user_mail, AOEtime=get_AOE_today(to_str=False))
-        monthly_counts = FileModel.get_interval_upload_count_by_mail(email=user_mail, AOEtime=get_AOE_month(to_str=False))
-        if (daily_counts >= configs["DAILY_SUBMIT_LIMIT"]) or (monthly_counts >= configs["MONTHLY_SUBMIT_LIMIT"]):
-            return {"msg": f"You have submitted {daily_counts} times today and {monthly_counts} times this month."}, HTTPStatus.FORBIDDEN
-
-        # TODO: Form Validation
-        submitName = request.form.get('submitName')
-        modelURL = request.form.get('modelURL')
-        modelDesc = request.form.get('modelDesc')
-        stride = request.form.get('stride')
-        inputFormat = request.form.get('inputFormat')
-        corpus = request.form.get('corpus')
-        paramDesc = request.form.get('paramDesc')
-        paramShared = request.form.get('paramShared')
-        fineTunedParam = request.form.get('fineTunedParam')
-        taskSpecParam = request.form.get('taskSpecParam')
-        task = request.form.get('task')
-        task = Task(mapping[task])
-        file = request.files['file']
-        if not (submitName and modelDesc and paramDesc):
-            return {"msg": "Column Missing."}, HTTPStatus.FORBIDDEN
-
-        if file.filename == "":
-            return {"msg": "No file selected."}, HTTPStatus.FORBIDDEN
-        if not file_upload.zipfile_check(file):
-            return {"msg": "Wrong file format."}, HTTPStatus.FORBIDDEN
-
-        upload_count = FileModel.get_upload_count_by_mail(email=user_mail) + 1
-
-        folder = file_upload.create_folder(user_mail, str(upload_count))
-        file_path = file_upload.get_full_path(folder, file.filename)
-
-        submitUUID = get_uuid()
-
-        fileObj = FileModel(
-            email=user_mail,
-            submitUUID=submitUUID,
-            submitName=submitName,
-            modelURL=modelURL,
-            modelDesc=modelDesc,
-            stride=stride,
-            inputFormat=inputFormat,
-            corpus=corpus,
-            paramDesc=paramDesc,
-            paramShared=paramShared,
-            fineTunedParam=fineTunedParam,
-            taskSpecParam=taskSpecParam,
-            task=task,
-            filePath=file_path,
-            aoeTimeUpload=get_AOETime()
-        )
-        scoreObj = ScoreModel()
-        fileObj.scores.append(scoreObj)
-        fileObj.save_to_db()
-
-        try:
-            file.save(file_path)
-
-            # start processing
-            thread = Thread(target=metric_calculate_pipeline, kwargs={"file_path":file_path,
-                                                                      "submitUUID":submitUUID})
-            thread.start()
-
-            return {"msg": "Upload Success!"}, HTTPStatus.OK
-        except Exception as e:
-            fileObj.delete_from_db()  # Rollback
-            return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
-    except Exception as e:
-        return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 @app.route("/api/profile/quota", methods=["GET"])
 @jwt_required()
 def get_user_quota():
     try:
         user_mail = get_jwt_identity()
-        daily_counts = FileModel.get_interval_upload_count_by_mail(email=user_mail, AOEtime=get_AOE_today(to_str=False))
-        monthly_counts = FileModel.get_interval_upload_count_by_mail(email=user_mail, AOEtime=get_AOE_month(to_str=False))
+        daily_counts = FileModel.get_interval_upload_count_by_mail(
+            email=user_mail, AOEtime=get_AOE_today(to_str=False))
+        monthly_counts = FileModel.get_interval_upload_count_by_mail(
+            email=user_mail, AOEtime=get_AOE_month(to_str=False))
         daily_left = configs["DAILY_SUBMIT_LIMIT"] - daily_counts
         monthly_left = configs["MONTHLY_SUBMIT_LIMIT"] - monthly_counts
-        return {"mgs": "Your quota:", "daily_counts":daily_counts, "monthly_counts":monthly_counts, "daily_left":daily_left, "monthly_left":monthly_left}, HTTPStatus.OK
+        return {"mgs": "Your quota:", "daily_counts": daily_counts, "monthly_counts": monthly_counts, "daily_left": daily_left, "monthly_left": monthly_left}, HTTPStatus.OK
 
     except Exception as e:
         print(e)
         return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
+
 
 @app.route("/api/profile/username", methods=["GET"])
 @jwt_required()
@@ -234,6 +122,7 @@ def get_username():
     except Exception as e:
         print(e)
         return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
+
 
 @app.route("/api/profile/resetusername", methods=["POST"])
 @jwt_required()
@@ -252,6 +141,7 @@ def reset_username():
         print(e)
         return {"msg": "Internal Server Error!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
+
 @app.route("/api/download/example", methods=['GET'])
 def download_example():
     try:
@@ -260,6 +150,8 @@ def download_example():
     except Exception as e:
         return {"message": "Something went wrong!"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
+
+api.add_resource(Result, "/api/result")
 
 if __name__ == '__main__':
     db.init_app(app)
