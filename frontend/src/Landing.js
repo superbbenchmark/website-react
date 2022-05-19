8bf32a21ce7a00e37fa6c08eac7f651662e99446
Joseph Feng <b06901052@ntu.edu.tw>
2022-05-20 00:30:19 +0800

    add tws logo

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -173,7 +173,8 @@ export default function Landing(props) {
                             ["fair-1000.png", "https://ai.facebook.com/"],
                             ["lxt-1000.png", "https://www.lxt.ai/"],
                             ["huggingface-1000.png", "https://huggingface.co/"],
-                            ["isca_logo_1000.png", "https://www.isca-speech.org/iscaweb/"]
+                            ["isca_logo_1000.png", "https://www.isca-speech.org/iscaweb/"],
+                            ["tws-1000.png", "https://tws.twcc.ai/"]
                         ].map((filename) => {
                             return (
                                 <Grid item xs={6} sm={4} md={4} key={filename[0]}>
